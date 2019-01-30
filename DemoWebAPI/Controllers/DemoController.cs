a25a246c50432d094dc2c8b7053b5c4594a1fc60
Albert <albert.shen@git4u.com>
2019-01-30 11:57:32 +0800

    Demo Web

---
+++ DemoWebAPI/Controllers/DemoController.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Http;
+
+namespace DemoWebAPI.Controllers
+{
+    /// <summary>
+    /// DemoController
+    /// </summary>
+    /// <seealso cref="System.Web.Http.ApiController" />
+    public class DemoController : ApiController
+    {
+        /// <summary>
+        /// Gets the demo.
+        /// </summary>
+        /// <returns>
+        /// IHttpActionResult
+        /// </returns>
+        [HttpGet]
+        [Route("Demo")]
+        public IHttpActionResult GetDemo()
+        {
+            return this.Ok("Hello World!");
+        }
+    }
+}
---
+++ DemoWebAPI/Global.asax.cs
@@ -0,0 +1,19 @@
+﻿using System.Web.Http;
+
+namespace DemoWebAPI
+{
+    /// <summary>
+    /// WebApiApplication
+    /// </summary>
+    /// <seealso cref="System.Web.HttpApplication" />
+    public class WebApiApplication : System.Web.HttpApplication
+    {
+        /// <summary>
+        /// Applications the start.
+        /// </summary>
+        protected void Application_Start()
+        {
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+        }
+    }
+}
